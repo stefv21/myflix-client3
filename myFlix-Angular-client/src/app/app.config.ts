24610f626cea2baa6228989f0b5d22f8343a6d3f
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-09-14 10:27:17 -0400

    updated my code with TS

--- myFlix-Angular-client/src/app/app.config.ts
+++ myFlix-Angular-client/src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(
+      MatInputModule,
+      MatButtonModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatDialogModule,
+      MatSnackBarModule,
+      FormsModule
+    )
+  ]
 };
